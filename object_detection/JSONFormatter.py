3ee6df406c8abbc8d7382785d4a509e29bb9be4f
KimHeeSu linux <wlskaka4@gmail.com>
2021-07-07 09:16:00 +0900

    JsonFormatter, keras-retinanet

--- object_detection/JSONFormatter.py
+++ object_detection/JSONFormatter.py
@@ -4,7 +4,7 @@ import uuid
 def jsonFormatter(box,label,img_name):
     b = box.astype(int)
     detected = {
-        "objectID": uuid.uuid4(),
+        "objectID": str(uuid.uuid4()),
         "location":{
             "xmin":b[1],
             "ymin":b[3],
@@ -18,5 +18,11 @@ def jsonFormatter(box,label,img_name):
     filename = output_path + "{}_path: ({}).jpg".format(labels_to_names_seq[label]+str(labels_to_num[label]),imagePath_str)
     with open(filename,"w") as outFile:
         json.dump(detected, outFile, indent=4)
-    
-    
+
+    print("hello")
+
+box = [1,2,3,4]
+label = "testLabel"
+img_name = "testImg"
+
+jsonFormatter(box,label,img_name)
